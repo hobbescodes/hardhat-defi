6a8acce3e6b0c479c31ba89b9562cf6e031fce93
Blueskunk22 <jakob.hauble@gmail.com>
2022-06-28 19:10:56 -0500

    add aave deposit functionality

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Deposit collateral - wETH
+2. Borrow another asset - DAI
+3. Repay DAI
+
+TradeOffs of using a forked mainnet network for testing:
+
+Pros: Quick, easy, resemble what's on mainnet
+Cons: We need an API, some contracts are complex to work with
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,62 +1,72 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require("hardhat-deploy");
-require("solidity-coverage");
-require("hardhat-gas-reporter");
-require("hardhat-contract-sizer");
-require("dotenv").config();
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
 
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const KOVAN_RPC_URL =
-  process.env.KOVAN_RPC_URL ||
-  "https://eth-mainnet.alchemyapi.io/v2/your-api-key";
+    process.env.KOVAN_RPC_URL || "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const RINKEBY_RPC_URL =
-  process.env.RINKEBY_RPC_URL ||
-  "https://eth-mainnet.alchemyapi.io/v2/your-api-key";
+    process.env.RINKEBY_RPC_URL || "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const MAINNET_RPC_URL =
+    process.env.MAINNET_RPC_URL || "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY =
-  process.env.PRIVATE_KEY ||
-  "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a";
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+    process.env.PRIVATE_KEY || "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 31337,
-      blockConfirmations: 1,
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            forking: {
+                url: MAINNET_RPC_URL,
+            },
+        },
+        rinkeby: {
+            chainId: 4,
+            blockConfirmations: 6,
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
     },
-    rinkeby: {
-      chainId: 4,
-      blockConfirmations: 6,
-      url: RINKEBY_RPC_URL,
-      accounts: [PRIVATE_KEY],
+    solidity: {
+        compilers: [
+            {
+                version: "0.4.19",
+            },
+            {
+                version: "0.6.12",
+            },
+        ],
     },
-  },
-  solidity: "0.8.8",
-  namedAccounts: {
-    deployer: {
-      default: 0,
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
     },
-    player: {
-      default: 1,
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
-  },
-  gasReporter: {
-    enabled: true,
-    currency: "USD",
-    outputFile: "gas-report.txt",
-    noColors: true,
-    coinmarketcap: COINMARKETCAP_API_KEY,
-  },
-  mocha: {
-    timeout: 1500000, // this is used when we are waiting for Promises
-  },
-  etherscan: {
-    apiKey: {
-      rinkeby: ETHERSCAN_API_KEY,
+    mocha: {
+        timeout: 1500000, // this is used when we are waiting for Promises
     },
-  },
-};
+    etherscan: {
+        apiKey: {
+            rinkeby: ETHERSCAN_API_KEY,
+        },
+    },
+}
