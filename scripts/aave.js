847c443375d7435c3902721eae9c77c0d49bac5f
Blueskunk22 <jakob.hauble@gmail.com>
2022-06-28 21:53:05 -0500

    add aave borrow dai functionality

--- scripts/aave.js
+++ scripts/aave.js
@@ -9,14 +9,26 @@ const main = async () => {
     const lendingPool = await getLendingPool(deployer)
     console.log(`LendingPool address: ${lendingPool.address}`)
 
+    // DEPOSIT
     const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
     await approveERC20(wethTokenAddress, lendingPool.address, AMOUNT, deployer)
 
     console.log("Depositing...")
     await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
     console.log("Deposited!")
+
+    // BORROW
+    let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(lendingPool, deployer)
+    const daiPrice = await getDaiPrice()
+    const amountDaiToBorrow = availableBorrowsETH.toString() * 0.95 * (1 / daiPrice.toNumber())
+    console.log(`You can borrow ${amountDaiToBorrow} DAI`)
+    const amountDaiToBorrowWei = ethers.utils.parseEther(amountDaiToBorrow.toString()) // need the amount in wai
+    const daiTokenAddress = "0x6b175474e89094c44da98b954eedeac495271d0f"
+    await borrowDai(daiTokenAddress, lendingPool, amountDaiToBorrowWei, deployer)
+    await getBorrowUserData(lendingPool, deployer)
 }
 
+// get the lendingPool address from the provider (provide ABI for AddressProvider, its address, and an account to lend from)
 const getLendingPool = async (account) => {
     const lendingPoolAddressProvider = await ethers.getContractAt(
         "ILendingPoolAddressesProvider",
@@ -28,6 +40,7 @@ const getLendingPool = async (account) => {
     return lendingPool
 }
 
+// in order for contracts to do a safeTransferFrom we most approve an amount to be withdrawn from our account
 const approveERC20 = async (erc20Address, spenderAddress, amountToSpend, account) => {
     const erc20Token = await ethers.getContractAt("IERC20", erc20Address, account)
 
@@ -35,6 +48,34 @@ const approveERC20 = async (erc20Address, spenderAddress, amountToSpend, account
     await tx.wait(1)
 
     console.log("Approved!")
+}
+
+// There is a function in the lendingPool contract that you can call that will provider import info
+const getBorrowUserData = async (lendingPool, account) => {
+    const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+        await lendingPool.getUserAccountData(account)
+    console.log(`Total Collateral: ${totalCollateralETH} ETH`)
+    console.log(`Total Debt: ${totalDebtETH} ETH`)
+    console.log(`Available to Borrow: ${availableBorrowsETH} ETH`)
+    return { availableBorrowsETH, totalDebtETH }
+}
+
+// Get the conversion rate for ETH / DAI
+const getDaiPrice = async () => {
+    const daiEthPriceFeed = await ethers.getContractAt(
+        "AggregatorV3Interface",
+        "0x773616E4d11A78F511299002da57A0a94577F1f4"
+    )
+    // once the function returns grab the first index which is the answer (price)
+    const price = (await daiEthPriceFeed.latestRoundData())[1]
+    console.log(`The DAI / ETH price is ${price.toString()}`)
+    return price
+}
+
+const borrowDai = async (daiAddress, lendingPool, amountDaiToBorrowWei, account) => {
+    const borrowTx = await lendingPool.borrow(daiAddress, amountDaiToBorrowWei, 1, 0, account)
+    await borrowTx.wait(1)
+    console.log(`Youve borrowed!`)
 }
 
 main()
